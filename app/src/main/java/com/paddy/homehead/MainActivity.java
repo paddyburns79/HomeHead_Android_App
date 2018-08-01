4326537c1f4d9582422a33c60a5d9c318b1ee94c
paddyburns79 <paddy_burns@hotmail.com>
2018-08-01 13:32:38 +0100

    Addition of Activities

--- app/src/main/java/com/paddy/homehead/MainActivity.java
+++ app/src/main/java/com/paddy/homehead/MainActivity.java
@@ -1,18 +1,11 @@
 package com.paddy.homehead;
 
-import android.os.AsyncTask;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
 
 
 public class MainActivity extends AppCompatActivity  {
@@ -26,109 +19,27 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button btnStart = findViewById(R.id.button_start_listen);
+        Button btnStart = findViewById(R.id.button_start_listen_activity);
         btnStart.setOnClickListener(new View.OnClickListener() {
-            //start execution of ssh commands
+
             @Override
-            public void onClick(View v){
-                new AsyncTask<Integer, Void, Void>(){
-                    @Override
-                    protected Void doInBackground(Integer... params) {
-                        try {
-                            executeSSHcommandStart();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                }.execute(1);
+            public void onClick(View view) {
+                Intent startListenIntent = new Intent(MainActivity.this, StartDeviceListeningActivity.class);
+                startActivity(startListenIntent);
             }
         });
 
-
-        Button btnStop = findViewById(R.id.button_stop_listen);
+        Button btnStop = findViewById(R.id.button_stop_listen_activity);
         btnStop.setOnClickListener(new View.OnClickListener() {
-            //start execution of ssh commands
+
             @Override
-            public void onClick(View v){
-                new AsyncTask<Integer, Void, Void>(){
-                    @Override
-                    protected Void doInBackground(Integer... params) {
-                        try {
-                            executeSSHcommandStop();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                }.execute(1);
+            public void onClick(View view) {
+                Intent stopListenIntent = new Intent(MainActivity.this, StopDeviceListeningActivity.class);
+                startActivity(stopListenIntent);
             }
         });
 
     }
-
-    public void executeSSHcommandStart(){
-        String user = "pi";
-        String password = "raspberry";
-        String host = "192.168.1.73";
-        int port=22;
-        try{
-
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, port);
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setTimeout(10000);
-            session.connect();
-            ChannelExec channel = (ChannelExec)session.openChannel("exec");
-            channel.setCommand("cd sopare; ./sopare.py -l");
-            channel.connect();
-            //channel.disconnect();
-            // Snackbar to indicate connection status : success
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Connection Successful!", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }
-        catch(JSchException e){
-            // Snackbar to indicate connection status (failure) and show the error in the UI
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Connection Error : "+e.getMessage(),
-                    Snackbar.LENGTH_LONG)
-                    .setDuration(20000).setAction("Action", null).show();
-        }
-    }
-
-    public void executeSSHcommandStop(){
-        String user = "pi";
-        String password = "raspberry";
-        String host = "192.168.1.73";
-        int port=22;
-        try{
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, port);
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setTimeout(10000);
-            session.connect();
-            ChannelExec channel = (ChannelExec)session.openChannel("exec");
-            channel.setCommand("killall python");
-            channel.connect();
-            channel.disconnect();
-            // Snackbar to indicate connection status : success
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Process Successfully Stopped!", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }
-        catch(JSchException e){
-            // Snackbar to indicate connection status (failure) and show the error in the UI
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Connection Error : "+e.getMessage(),
-                    Snackbar.LENGTH_LONG)
-                    .setDuration(20000).setAction("Action", null).show();
-        }
-    }
-
 
 
     @Override
---
+++ app/src/main/java/com/paddy/homehead/StopDeviceListeningActivity.java
@@ -0,0 +1,72 @@
+package com.paddy.homehead;
+
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class StopDeviceListeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_device_listening);
+
+        Button btnStart = findViewById(R.id.button_stop_listen);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            //start execution of ssh commands
+            @Override
+            public void onClick(View v){
+                new AsyncTask<Integer, Void, Void>(){
+                    @Override
+                    protected Void doInBackground(Integer... params) {
+                        try {
+                            executeSSHcommandStart();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.execute(1);
+            }
+        });
+    }
+
+    public void executeSSHcommandStart(){
+        String user = "pi";
+        String password = "raspberry";
+        String host = "192.168.1.73";
+        int port=22;
+        try{
+
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setTimeout(10000);
+            session.connect();
+            ChannelExec channel = (ChannelExec)session.openChannel("exec");
+            channel.setCommand("killall python");
+            channel.connect();
+            //channel.disconnect();
+            // Snackbar to indicate connection status : success
+            Snackbar.make(findViewById(android.R.id.content),
+                    "Successfully Stopped!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        catch(JSchException e){
+            // Snackbar to indicate connection status (failure) and show the error in the UI
+            Snackbar.make(findViewById(android.R.id.content),
+                    "Connection Error : "+e.getMessage(),
+                    Snackbar.LENGTH_LONG)
+                    .setDuration(20000).setAction("Action", null).show();
+        }
+    }
+}
